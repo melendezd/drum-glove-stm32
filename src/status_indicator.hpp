69c66aa81cd5b3f7c08f5c39b713f51c9f66f76e
David Melendez <david.m.melendez@outlook.com>
2024-03-27 08:55:57 -0400

    add some docs to StatusIndicator

--- src/status_indicator.hpp
+++ src/status_indicator.hpp
@@ -3,11 +3,25 @@
 #include "io.hpp"
 #include "timer.hpp"
 
+// Allows you to toggle an output to a GPIO pin indicating a status code.
+// Useful for indicating errors.
+//
+// Dependencies:
+// - GPIO out: Pin to which the status will be output. Recommended to use an LED
+// - DelayTimer delay: Delay timer which will be used for timing in the output pattern.
 class StatusIndicator
 {
   public:
     StatusIndicator( GPIO &out, DelayTimer &delay );
+
+    // This function will loop forever.
+    //
+    // Toggles the output pin in a pattern to indicate the status code forever.
+    // The pattern is: One long blink, and then `status_code` short blinks
     void status( uint32_t status_code );
+
+    // Toggles the output pin in a pattern to indicate the status code once.
+    // The pattern is: One long blink, and then `status_code` short blinks
     void status_once( uint32_t status_code );
 
   private:
